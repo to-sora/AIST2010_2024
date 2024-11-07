7f9940140f5863d8daf00de2dbdde312b5a20b0b
to-sora <to-sora@users.noreply.github.com>
2024-11-07 23:23:26 +0800

    ok

--- project/config.py
+++ project/config.py
@@ -2,7 +2,7 @@
 import torch
 
 CONFIG = {
-    'version': 'v1',
+    'version': 'v2',
     'data_dir': './data_2/',
     'cache_dir': './data/cache/',
     'logs_dir': './logs/',
@@ -11,8 +11,8 @@ CONFIG = {
     'num_workers': 2,
     'lr': 1e-4,
     'weight_decay': 1e-4,
-    'epochs': 50,
-    'gradient_accumulation_steps': 8,
+    'epochs': 100,
+    'gradient_accumulation_steps': 32,
     'device': 'cuda' if torch.cuda.is_available() else 'cpu',
     'model_save_path': './models/model_data',
     'num_classes': {
@@ -23,14 +23,14 @@ CONFIG = {
     'model_structure': {
         "dimension": 128,
         "num_encoder_layers": 1,
-        "num_decoder_layers": 2,
+        "num_decoder_layers": 1
 
     },
     'max_objects': 100 ,       # Maximum notes per audio sample
     "cost_note_type": 1,
     "cost_instrument": 1,
     "cost_pitch": 5,
-    "cost_regression": 1,
+    "cost_regression": 3,
     "data_nftt":4096,
     "data_win_length":4096,
     "data_hop_length":256,
